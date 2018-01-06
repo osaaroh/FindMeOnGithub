8f563c925f09c3d1c87f2e3f3c9d674653076ae3
Osaro Iyoha <saro.iyoha@gmail.com>
2018-01-06 01:42:06 +0100

    Add splashscreen activity

---
+++ app/src/main/java/com/genesis/sahx/fmogithub/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.genesis.sahx.fmogithub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
