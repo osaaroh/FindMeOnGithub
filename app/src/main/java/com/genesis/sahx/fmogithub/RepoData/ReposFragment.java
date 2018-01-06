00d58154496e3c1aa5179f448238691329c0a74e
Osaro Iyoha <saro.iyoha@gmail.com>
2018-01-06 15:08:05 +0100

    Add progress dialog when userdata loads

--- app/src/main/java/com/genesis/sahx/fmogithub/RepoData/ReposFragment.java
+++ app/src/main/java/com/genesis/sahx/fmogithub/RepoData/ReposFragment.java
@@ -1,6 +1,6 @@
 package com.genesis.sahx.fmogithub.RepoData;
 
-
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
