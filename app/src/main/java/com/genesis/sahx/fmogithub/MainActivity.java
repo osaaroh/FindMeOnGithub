03662a9928987761d587246d184f6a268539c3b5
Osaro Iyoha <saro.iyoha@gmail.com>
2018-01-13 14:00:16 +0100

    Update MainActivity.java

--- app/src/main/java/com/genesis/sahx/fmogithub/MainActivity.java
+++ app/src/main/java/com/genesis/sahx/fmogithub/MainActivity.java
@@ -1,7 +1,13 @@
 package com.genesis.sahx.fmogithub;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -9,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,19 +24,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //FirstLayout.setVisibility(View.VISIBLE);
-        Button button = (Button)findViewById(R.id.mainButton);
+        Button button = (Button) findViewById(R.id.mainButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //code to pass input data from edit text in this activity to another activity
-                EditText editText = (EditText)findViewById(R.id.username);
-                String name = editText.getText().toString();
-                startActivity(new Intent(MainActivity.this, TabbedMainActivity.class).putExtra("username",name));
-                Log.w("MainActivity","Will pass Username to TabbedActivity: "+ name);
+                if (isOnline()){
+                    EditText editText = (EditText) findViewById(R.id.username);
+                    if (editText.getText().toString().trim().equals("")) {
+                        Toast.makeText(MainActivity.this, "You didn't enter a Github Username", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        String name = editText.getText().toString().trim();
+                        startActivity(new Intent(MainActivity.this, TabbedMainActivity.class).putExtra("username", name));
+                        Log.w("MainActivity", "Will pass Username to TabbedActivity: " + name);
+                    }
+                }
+                else
+                {
+                    try {
+                        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                        alertDialog.setTitle("Info");
+                        alertDialog.setMessage("Internet connectivity not available. Check your internet connection and try again");
+                        alertDialog.setIcon(android.R.drawable.ic_dialog_alert);
+                        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+                        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "WIFI", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                            }
+                        });
+
+                        alertDialog.show();
+                    }
+                    catch(Exception e)
+                    {
+                        //ToDo
+                    }
+                }
+
             }
         });
+
+        //FirstLayout.setVisibility(View.VISIBLE);
+
     }
+
+    public boolean isOnline() {
+        ConnectivityManager conMgr = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = conMgr.getActiveNetworkInfo();
+
+        if(netInfo == null || !netInfo.isConnected() || !netInfo.isAvailable()){
+            //Toast.makeText(this, "No Internet connection!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -46,9 +102,13 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+        }
+        if (id == R.id.action_about) {
+            startActivity(new Intent(MainActivity.this, AboutActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
